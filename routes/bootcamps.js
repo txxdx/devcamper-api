48029b6028d9c75abd78982e387877d4278fc26f
huangxiaobin <huangxiaobin@cmss.chinamobile.com>
2024-05-31 13:41:19 +0800

    add courses get request

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,6 +9,12 @@ const {
 	getBootcampsInRadius,
 } = require('../controllers/bootcamps')
 
+// Include other resource routers
+const courseRouter = require('./courses')
+
+// Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter)
+
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius)
 router.route('/').get(getBootcamps).post(createBootcamp)
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp)
