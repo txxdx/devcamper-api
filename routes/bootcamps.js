17cba3d1460d34dcfe66522f5858bd1f2fa64ee5
huangxiaobin <huangxiaobin@cmss.chinamobile.com>
2024-06-07 17:36:06 +0800

    add roles authorize

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -14,6 +14,8 @@ const Bootcamp = require('../models/Bootcamp')
 
 const advancedResults = require('../middleware/advancedResults')
 
+const { protect, authorize } = require('../middleware/auth')
+
 // Include other resource routers
 const courseRouter = require('./courses')
 
@@ -22,12 +24,18 @@ router.use('/:bootcampId/courses', courseRouter)
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius)
 
-router.route('/:id/photo').put(bootcampPhotoUpload)
+router
+	.route('/:id/photo')
+	.put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload)
 
 router
 	.route('/')
 	.get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-	.post(createBootcamp)
-router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp)
+	.post(protect, authorize('publisher', 'admin'), createBootcamp)
+router
+	.route('/:id')
+	.get(getBootcamp)
+	.put(protect, authorize('publisher', 'admin'), updateBootcamp)
+	.delete(protect, authorize('publisher', 'admin'), deleteBootcamp)
 
 module.exports = router
--- routes/courses.js
+++ routes/courses.js
@@ -12,6 +12,8 @@ const {
 const Course = require('../models/Course')
 const advancedResults = require('../middleware/advancedResults')
 
+const { protect, authorize } = require('../middleware/auth')
+
 router
 	.route('/')
 	.get(
@@ -21,7 +23,11 @@ router
 		}),
 		getCourses
 	)
-	.post(addCourse)
-router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse)
+	.post(protect, authorize('publisher', 'admin'), addCourse)
+router
+	.route('/:id')
+	.get(getCourse)
+	.put(protect, authorize('publisher', 'admin'), updateCourse)
+	.delete(protect, authorize('publisher', 'admin'), deleteCourse)
 
 module.exports = router
