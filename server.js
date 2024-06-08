f966548a78acd3d4fc75fa035fcb3c3de6217378
huangxiaobin <huangxiaobin@cmss.chinamobile.com>
2024-06-08 19:28:39 +0800

    Add security packages

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv')
 const morgan = require('morgan')
 const colors = require('colors')
 const sanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
+const xss = require('xss-clean')
 const cookieParser = require('cookie-parser')
 const fileupload = require('express-fileupload')
 const errorHandler = require('./middleware/error')
@@ -42,6 +44,12 @@ app.use(fileupload())
 
 // Sanitize data
 app.use(sanitize())
+
+// Set security headers
+app.use(helmet())
+
+// Prevent XSS attacks
+app.use(xss())
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
