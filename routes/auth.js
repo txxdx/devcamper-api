30f248506e24943b6c61d00a38868e52cfa506e9
huangxiaobin <huangxiaobin@cmss.chinamobile.com>
2024-06-08 14:21:06 +0800

    update user detail

--- routes/auth.js
+++ routes/auth.js
@@ -9,12 +9,16 @@ const {
 	getMe,
 	forgotPassword,
 	resetPassword,
+	updateDetails,
+	updatePassword
 } = require('../controllers/auth')
 
 router.route('/register').post(register)
 router.route('/login').post(login)
 router.route('/me').get(protect, getMe)
+router.route('/updatedetails').put(protect, updateDetails)
 router.route('/forgotpassword').post(forgotPassword)
 router.route('/resetpassword/:resettoken').put(resetPassword)
+router.route('/updatepassword').put(protect, updatePassword)
 
 module.exports = router
