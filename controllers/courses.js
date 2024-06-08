eb3b5e1941dc68f4a528006a8fd503264a40ba82
huangxiaobin <huangxiaobin@cmss.chinamobile.com>
2024-06-08 11:54:08 +0800

    add forgot password and reset password

--- controllers/courses.js
+++ controllers/courses.js
@@ -48,12 +48,22 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
 // @access: Private
 exports.addCourse = asyncHandler(async (req, res, next) => {
 	req.body.bootcamp = req.params.bootcampId
+	req.body.user = req.user.id
+
 	const bootcamp = await Bootcamp.findById(req.params.bootcampId)
 
 	if (!bootcamp) {
 		new ErrorResponse(
 			`No bootcamp with the id of ${req.params.bootcampId}`,
 			404
+		)
+	}
+
+	// Make sure user is bootcamp owner
+	if (bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+		new ErrorResponse(
+			`User ${req.user.id} is not authorized to add a course to bootcamp ${bootcamp._id}`,
+			401
 		)
 	}
 
@@ -75,6 +85,14 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
 		new ErrorResponse(`No course with the id of ${req.params.id}`, 404)
 	}
 
+	// Make sure user is course owner
+	if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+		new ErrorResponse(
+			`User ${req.user.id} is not authorized to update course ${course._id}`,
+			401
+		)
+	}
+
 	course = await Course.findByIdAndUpdate(req.params.id, req.body, {
 		new: true,
 		runValidators: true,
@@ -94,6 +112,14 @@ exports.deleteCourse = asyncHandler(async (req, res, next) => {
 
 	if (!course) {
 		new ErrorResponse(`No course with the id of ${req.params.id}`, 404)
+	}
+
+	// Make sure user is course owner
+	if (course.user.toString() !== req.user.id && req.user.role !== 'admin') {
+		new ErrorResponse(
+			`User ${req.user.id} is not authorized to delete course ${course._id}`,
+			401
+		)
 	}
 
 	// await course.remove()
--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,18 @@ const router = express.Router()
 
 const { protect } = require('../middleware/auth')
 
-const { register, login, getMe } = require('../controllers/auth')
+const {
+	register,
+	login,
+	getMe,
+	forgotPassword,
+	resetPassword,
+} = require('../controllers/auth')
 
 router.route('/register').post(register)
 router.route('/login').post(login)
 router.route('/me').get(protect, getMe)
+router.route('/forgotpassword').post(forgotPassword)
+router.route('/resetpassword/:resettoken').put(resetPassword)
 
 module.exports = router
